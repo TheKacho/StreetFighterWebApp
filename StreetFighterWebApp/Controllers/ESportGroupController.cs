83772e74121329a59bc9daf2b83b72a554c4bfc3
TheKacho <abyssknightzero@gmail.com>
2022-06-23 01:51:27 -0400

    created controllers and the index pages for main index, esport group and tournament

---
+++ StreetFighterWebApp/Controllers/ESportGroupController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StreetFighterWebApp.Controllers
+{
+    public class ESportGroupController : Controller
+    {
+        public IActionResult Index() 
+        {
+            return View();
+        }
+    }
+}
